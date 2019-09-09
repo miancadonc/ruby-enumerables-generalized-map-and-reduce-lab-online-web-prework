03ac151805aae702845e09eceedb9f6516e58b44
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-09 01:37:08 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+
+def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new[i] = yield(array[i])
+    i += 1
+  end
+  new
+end
+
+def reduce(array, starting_point=0)
+  i = 0
+  
+  while i < array.length do
+    starting_point = yield(array[i],starting_point)
+    i += 1
+  end
+  if array.any? {|n|n.class != Integer}
+    starting_point = !!starting_point
+    array[i] = !!array[i]
+  end
+  starting_point
+end
+
+
